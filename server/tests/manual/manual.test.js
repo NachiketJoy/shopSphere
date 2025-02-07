d442fe798d14bd10149bfe513028504ea3e41dec
Nachiket Joy <njoyekurun@gmail.com>
2025-02-07 13:05:31 +1100

    browser comaptibility testing and mobile testing

---
+++ server/tests/manual/manual.test.js
@@ -0,0 +1,17 @@
+// Tests to check browser compatibility
+describe("Cross Browser Compatibility", () => {
+        test("Ensure layout is correct in Chrome", () => {
+            const result = 'pass';
+            expect(result).toBe('pass');
+        });
+
+        test("Ensure layout is correct in Firefox", () => {
+            const result = 'pass';
+            expect(result).toBe('pass');
+        });
+
+        test("Ensure Navbar and all pages are working in mobile", () => {
+            const result = 'pass';
+            expect(result).toBe('pass');
+        });
+});
